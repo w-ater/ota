b3d870013d4f4dcf8b1967fddbed45c0e135a233
hxq <809158675@qq.com>
2024-05-08 17:40:26 +0800

    改成从天翼云下载升级包

--- ota.c
+++ ota.c
@@ -10,6 +10,14 @@
 #include <sys/stat.h>
 #include <pthread.h>
 #include <sys/time.h>
+#include <time.h>
+#include <signal.h>
+
+#define SERIAL_PORT "/dev/ttyUSB2"
+#define BUFFER_SIZE 256
+
+#define TIMEOUT_SEC 3
+
 /* 
 #define OTA_DEBUG //开启debug打印
 #ifdef OTA_DEBUG
@@ -32,10 +40,10 @@
 #define OTA_ERR(...) 
 #elif(LOG_LEVEL == INFO_LEVEL)
 #define OTA_INFO(...) fprintf(stdout, "[WS_INFO] %s(%d): ", __FUNCTION__, __LINE__),fprintf(stdout, __VA_ARGS__)
-#define OTA_ERR(...) fprintf(stderr, "[WS_ERR] %s(%d): ", __FUNCTION__, __LINE__),fprintf(stderr, __VA_ARGS__)
+#define OTA_ERR(...) fprintf(stderr, "[OTA_INFO] %s(%d): ", __FUNCTION__, __LINE__),fprintf(stderr, __VA_ARGS__)
 #elif(LOG_LEVEL == ERR_LEVEL)
 #define OTA_INFO(...) 
-#define OTA_ERR(...) fprintf(stderr, "[WS_ERR] %s(%d): ", __FUNCTION__, __LINE__),fprintf(stderr, __VA_ARGS__)
+#define OTA_ERR(...) fprintf(stderr, "[OTA_INFO] %s(%d): ", __FUNCTION__, __LINE__),fprintf(stderr, __VA_ARGS__)
 #endif
 
 struct resp_header//保持相应头信息
@@ -283,7 +291,7 @@ int  SetDevVer(const char* ver_in)
 void read_ini_node(const char *filename, const char *node) {
     FILE *file = fopen(filename, "r");
     if (file == NULL) {
-        printf("Error opening file\n");
+        OTA_INFO("Error opening file\n");
         return;
     }
 
@@ -303,7 +311,7 @@ void read_ini_node(const char *filename, const char *node) {
                 value = strtok(NULL, "=");
 
                 if (key != NULL && value != NULL) {
-                    printf("Key: %s, Value: %s\n", key, value);
+                    OTA_INFO("Key: %s, Value: %s\n", key, value);
                     count++;
 
 					OTA_INFO("!!!file_path%s\n",value);
@@ -318,7 +326,7 @@ void read_ini_node(const char *filename, const char *node) {
         }
     }
 
-    printf("Total number of key-value pairs in node %s: %d\n", node, count);
+    OTA_INFO("Total number of key-value pairs in node %s: %d\n", node, count);
 
     fclose(file);
 }
@@ -503,6 +511,188 @@ void * download(void * socket_d)
 #endif
 }
 #endif
+
+void * parseAndextract(void * arg)
+{
+    /*下载文件函数, 放在线程中执行*/
+    int length = 0;
+    int mem_size = 4096;//mem_size might be enlarge, so reset it
+    int buf_len = mem_size;//read 4k each time
+    int len;
+	char ver_str[64]={0};
+	char tmpVer[64]={0};
+
+	char ota_str[64]={0};
+	char md5_get[32]={0};
+
+    int ota_fd = open("/tmp/upgrade.img", O_RDONLY);
+    if (ota_fd < 0) {
+        OTA_ERR("Error opening upgrade.img file\n");
+        pthread_exit(NULL);
+    }
+
+    char *buf = (char *) malloc(mem_size * sizeof(char));
+	if (buf != NULL)
+	{
+	    // 内存分配成功
+	}
+	else
+	{
+	    OTA_ERR("Error allocating memory\n");
+		close(ota_fd);
+	    pthread_exit(NULL);
+	}
+
+	if ((len = read(ota_fd, buf, 64)) != 0)
+	{
+		if (len < 0) {
+	        OTA_ERR("Error reading from socket\n");
+	        free(buf);
+			close(ota_fd);
+	        pthread_exit(NULL);
+	    } else if (len < 64) {
+	        OTA_ERR("Incomplete data read\n");
+	        free(buf);
+			close(ota_fd);
+	        pthread_exit(NULL);
+	    }
+		memcpy(ota_str,buf,64);
+		ota_str[63]='\0';
+		OTA_INFO("!!!ota_str%s\n",ota_str);
+		
+		memcpy(ver_str,ota_str+3,7);
+		OTA_INFO("ver_str is %s\n",ver_str);
+		
+		GetDevVer(tmpVer);
+		OTA_INFO("tmpVer is %s\n",tmpVer);
+		
+		if (compare_versions(tmpVer, ver_str)) {
+			OTA_INFO("需要升级从 %s到版本 %s\n", tmpVer, ver_str);
+
+		} else {
+			if (strcmp(ver_str, "0.0.0.0") == 0){
+				OTA_ERR("版本%s 强制升级\n",ver_str);
+			}else{
+				OTA_ERR("当前版本已是最新\n");
+				free(buf);
+				close(ota_fd);
+				OTA_INFO("!!!reboot\n");
+				//system("reboot");	
+				//execl("/sbin/reboot", "reboot", NULL);
+				pthread_exit(NULL);
+			}
+		}
+
+		//SetDevVer(ver_str);
+		memset(md5_get, 0, sizeof(md5_get));
+		memcpy(md5_get,ota_str+10,32);
+
+		OTA_INFO("%s\n \n", md5_get);			
+
+        //write(fd, buf, len);
+        //progressBar(length, resp.content_length);
+    }
+	memset(buf,0,4096);
+	resp.content_length -= 64;
+
+	//创建文件描述符
+    int fd = open("/tmp/upgrade.tar.lzma", O_CREAT | O_WRONLY, S_IRWXG | S_IRWXO | S_IRWXU);
+    if (fd < 0)
+    {
+        OTA_ERR("Create file failed\n");
+		pthread_exit(NULL);
+    }
+    //从套接字中读取文件流
+    // while ((len = read(ota_fd, buf, buf_len)) != 0 && length < resp.content_length)
+   
+    while ((len = read(ota_fd, buf, buf_len)) != 0)
+	{
+		if (len != -1)
+		{
+			write(fd, buf, len);
+			length += len;
+			memset(buf,0,4096);
+			//progressBar(length, resp.content_length);
+		}
+		else
+		{
+		    OTA_ERR("Error reading data from socket\n");
+		    // 可以根据具体情况采取适当的处理措施
+		}
+    }
+	system("cp /tmp/upgrade.tar.lzma /media/mmcblk0/");
+ 	free(buf);
+	
+#if 1
+	int ret;
+	const char *file_upgrade = "upgrade.tar.lzma";
+	char md5_str[MD5_STR_LEN + 1];
+ 
+	ret = Compute_file_md5(file_upgrade, md5_str);
+	if (0 == ret)
+	{
+		OTA_INFO("[file - %s] md5 value:\n", file_upgrade);
+		OTA_INFO("%s\n", md5_str);
+	}
+	else
+	{
+	    OTA_ERR("Error computing file MD5\n");
+	    // 可以根据具体情况采取适当的处理措施
+	}
+	
+	if (strncasecmp(md5_get, md5_str, 32) == 0)
+	{
+		OTA_INFO("md5_str is fuhe\n");
+	}else{
+		OTA_ERR("md5_str is bufuhe\n");
+
+	}
+	
+	char SnOut[64]={0,0,0,0};
+	ret = readStringValue("ipc", "sn", SnOut, "/root/app.ini");
+	if (ret == 1)
+	{
+	    // 读取配置值成功
+	}
+	else
+	{
+	    OTA_ERR("Error reading configuration value\n");
+	    // 可以根据具体情况采取适当的处理措施
+	}
+	OTA_INFO("%c %c %c %c\n",SnOut[3] , ota_str[42],SnOut[4] , ota_str[43]);
+	if(SnOut[3] == ota_str[42]&&SnOut[4] == ota_str[43]){
+		OTA_INFO("SN fuhe!!!!!!\n");
+	}else{
+		OTA_ERR("SN bufuhe!!!!!!\n");
+		//execl("/sbin/reboot", "reboot", NULL);
+		pthread_exit(NULL);
+	}
+	
+	system("rm upgrade.tar");
+
+	system("lzma -d upgrade.tar.lzma");
+	system("./busybox tar xvf upgrade.tar -C /tmp");
+	
+	read_ini_node("/tmp/ota.ini", "file");
+
+	
+	ret = readStringValue("ota", "sv", ver_str, "/tmp/ota.ini");
+	if (ret == 1)
+	{
+	    // 读取配置值成功
+	}
+	else
+	{
+	    OTA_ERR("Error reading configuration value\n");
+	    // 可以根据具体情况采取适当的处理措施
+	}
+
+	//SetDevVer(ver_str);
+	OTA_ERR("!!!reboot\n");
+	//execl("/sbin/reboot", "reboot", NULL);
+	pthread_exit(NULL);
+#endif
+}
 
 int upgrade_from_card3(char *path)
 {
@@ -1052,11 +1242,359 @@ long getAvailableSpace()
     return -1; // 获取失败
 }
 
+int KillProcessByName(char *processName) 
+{
+    FILE *fp;
+    char buf[BUFFER_SIZE];
+	
+    char cmd[BUFFER_SIZE] = { 0 };
+	sprintf(cmd, "ps aux | grep %s | grep -v grep | awk '{print $1}'", processName);
+    // 使用popen调用ps命令获取进程信息
+    //fp = popen("ps aux | grep cat | grep -v grep | awk '{print $1}'", "r");
+	
+    fp = popen(cmd, "r");
+    if (fp == NULL) {
+        fprintf(stderr, "Error opening pipe!\n");
+        return -1;
+    }
+    // 逐行读取进程ID并杀死进程
+    while (fgets(buf, sizeof(buf), fp) != NULL) {
+		
+		OTA_INFO(" processName %s pid: %s\n",processName, buf);
+        int pid = atoi(buf);
+        if (pid > 0) {
+            //WS_INFO("Killing process with PID: %d\n", pid);
+            kill(pid, SIGKILL);
+        }
+    }
+
+    pclose(fp);
+
+    return 0;
+}
+
+int get4GSerialOutput(const char *strCmd, char *buffer) 
+{
+    char cmd[BUFFER_SIZE];
+    sprintf(cmd, "%s && cat /dev/ttyUSB2", strCmd);
+    
+    //sprintf(cmd, "cat /dev/ttyUSB2 & %s;",strCmd);
+    FILE *fp;
+    char buf[BUFFER_SIZE];
+    char tempBuf[BUFFER_SIZE] = "";
+	unsigned int msgLine = 0;
+
+    fp = popen(cmd, "r");
+    if (fp == NULL) {
+        fprintf(stderr, "Error opening pipe!\n");
+        return -1;
+    } else {
+         //OTA_INFO("strCmd %s success\n", strCmd);
+    }
+
+    int fd = fileno(fp);
+	
+    fd_set readfds;
+    struct timeval timeout;
+    FD_ZERO(&readfds);
+    FD_SET(fd, &readfds);
+
+    timeout.tv_sec = TIMEOUT_SEC;
+    timeout.tv_usec = 0;
+	//system(strCmd);
+
+	
+    while (1) {
+		int ret = select(fd + 1, &readfds, NULL, NULL, &timeout);
+	    if (ret == -1) {
+	        perror("select failed");
+	        KillProcessByName("cat");
+	        pclose(fp);
+	        return -2;
+	    } else if (ret == 0) {
+			if(msgLine > 1){
+				
+				OTA_INFO("msg: %s \n",tempBuf);
+				memcpy(buffer,tempBuf,BUFFER_SIZE);
+			
+				OTA_INFO("msgbuffer: %s \n",buffer);
+				KillProcessByName("cat");
+		        pclose(fp);				
+				return 0;
+			}else{				
+				OTA_INFO("Timeout occurred strCmd %s\n", strCmd);
+		        KillProcessByName("cat");
+		        pclose(fp);
+		        return -2;
+			}
+	    }else{
+			if (fgets(buf, sizeof(buf), fp) != NULL) {
+				if (strlen(buf) > 1) {
+					msgLine++;
+					OTA_INFO("Received data: msgLine:%d len:%d%s,%d\n",msgLine, strlen(buf),buf);
+					strcat(tempBuf, buf);
+					tempBuf[255]='\0';
+					//OTA_INFO("tempBuf %s \n",tempBuf);
+
+				}
+			}
+		}
+		
+        //usleep(10000);
+    }
+}
+int Get4GOutputBySaveFile(const char *strCmd, char *buffer) 
+{
+    char cmd[BUFFER_SIZE];
+    //sprintf(cmd, "%s && cat /dev/ttyUSB2", strCmd);
+    
+    //sprintf(cmd, "cat /dev/ttyUSB2 > ATOutput.txt &",strCmd);
+    FILE *fp;
+    char buf[BUFFER_SIZE];
+    char tempBuf[BUFFER_SIZE] = "";
+	unsigned int msgLine = 0;
 
 
+	
+	system("cat /dev/ttyUSB2 > ATOutput.txt &");
+	system(strCmd);
+	OTA_INFO("strCmd %s success\n", strCmd);
+	
+	fp = fopen("ATOutput.txt" , "r");
+	if(fp == NULL) {
+	   perror("打开文件时发生错误");
+	   return(-1);
+	}
+    while (fgets(buf, sizeof(buf), fp) != NULL) {
+        if (strlen(buf) > 1) {
+            msgLine++;
+            OTA_INFO("output_fd data: msgLine:%d len:%d%s,%d\n", msgLine, (int)strlen(buf), buf);
+			strcat(tempBuf, buf);
+			tempBuf[255]='\0';
+			
+            //if (msgLine >= 5) {
+            //    break; // 限制读取的行数
+            //}
+        }
+    }
+	
+	if(msgLine > 1){
+				
+		//WS_ERR("msg: %s \n",tempBuf);
+		memcpy(buffer,tempBuf,BUFFER_SIZE);
+	
+		OTA_INFO("msgbuffer: %s \n",buffer);
+		KillProcessByName("cat");
+        pclose(fp);				
+		return 0;
+	}else{				
+		OTA_INFO("Timeout occurred strCmd %s\n", strCmd);
+        KillProcessByName("cat");
+        pclose(fp);
+        return -1;
+	}
+
+
+}
+
+
+void myprint(const char *format, int timestamp) {
+    OTA_INFO(format, timestamp);
+    OTA_INFO("\n");
+}
+
+void executeCmd(const char *cmd) {
+    system(cmd);
+}
+int getATOutput(const char *strCmd, char *buffer) 
+{
+	
+	int ret;
+	int getCNT = 10;
+	while(getCNT--){
+		
+    	char buf[BUFFER_SIZE] = {0};
+		ret = Get4GOutputBySaveFile(strCmd,buf);
+		if(!ret){
+			
+			OTA_INFO("strCmd %s \n buf %s\n buffer is %s\n", strCmd, buf,buffer);
+			if (strstr(buf, buffer)) {
+				strcpy(buffer, buf);
+				OTA_INFO("strCmd %s buffer is %s\n", strCmd, buffer);
+				return 0;
+			} else if (strstr(buf, "OK")){
+				
+				OTA_INFO("OK\n");
+			} else if (strstr(buf, "ERROR")){
+				
+				OTA_INFO("ERROR\n");
+			}
+			
+			//return -1;
+
+		}else{
+			OTA_INFO("cant get ATOutput, try again\n");	
+		}
+	}
+	OTA_INFO("cant get ATOutput, please try again\n");
+}
+
+int getTime() {
+    // 获取时间 +CCLK: "24/01/09,07:41:32+32"
+    time_t timestamp = 0; // UTC: 2024-01-01 00:00:00
+    char buffer[1000] = {0};
+    getATOutput("echo -e 'AT+CCLK?\r\n' > /dev/ttyUSB2", buffer);
+    // Assuming getATOutput function is responsible for populating buffer with AT command output
+    //char *strSrc = "+CCLK: \"24/01/09,07:41:32+32\"";
+	
+    char *strSrc = buffer;
+    OTA_INFO("getTime: %s\n", strSrc);
+
+    int nposBegin = strchr(strSrc, '\"') - strSrc + 1;
+    int nposEnd = strchr(strSrc + nposBegin, '\"') - strSrc;
+    
+    if (strlen(strSrc) != 0 && nposBegin != -1 && nposEnd != -1) {
+        char strR[20];
+        strncpy(strR, strSrc + nposBegin, nposEnd - nposBegin);
+        strR[nposEnd - nposBegin] = '\0';
+
+        char strYear[3], strMon[3], strDate[3], strHour[3], strMin[3], strSec[3];
+        strncpy(strYear, strR, 2);
+        strncpy(strMon, strR + 3, 2);
+        strncpy(strDate, strR + 6, 2);
+        strncpy(strHour, strR + 9, 2);
+        strncpy(strMin, strR + 12, 2);
+        strncpy(strSec, strR + 15, 2);
+
+        struct tm timeinfo = {0};
+
+        // 设置本地时间信息
+        timeinfo.tm_year = atoi(strYear) + 100; // 年份加上 100，相当于 2000 年之后的年份
+        timeinfo.tm_mon = atoi(strMon) - 1; // 月份从 0 开始，0 表示一月
+        timeinfo.tm_mday = atoi(strDate); // 日期
+        timeinfo.tm_hour = atoi(strHour); // 小时
+        timeinfo.tm_min = atoi(strMin); // 分钟
+        timeinfo.tm_sec = atoi(strSec); // 秒钟
+
+        // 将本地时间转换为 UTC 时间
+        timestamp = mktime(&timeinfo);
+    }
+    myprint("getTime: %d", timestamp);
+    return timestamp;
+}
+
+void setTime(time_t nTime) {
+    struct tm *t;
+    time_t now;
+    if (nTime == 0) {
+        executeCmd("date -s \"2024-01-09 10:26\"");
+        time(&now);
+        t = localtime(&now);
+    } else {
+        now = nTime;
+        t = localtime(&now);
+    }
+    char strTime[100];
+    snprintf(strTime, 99, "date -s \"%d-%02d-%02d %02d:%02d:%02d\"", t->tm_year + 1900, t->tm_mon + 1, t->tm_mday,
+             t->tm_hour, t->tm_min, t->tm_sec);
+    executeCmd(strTime);
+}
+
+int mySetTimeFrom4G() {
+    for (size_t i = 0; i < 10; i++) {
+        time_t nTime = getTime();
+        if (nTime != 0) {
+            setTime(nTime);
+            break;
+        }
+        usleep(200 * 1000);
+    }
+    return 0;
+}
+
+
+
+//date -s "2024-05-08 06:34"
+//1.0.0.1876B885EA47289377C60E97B9C73EE6222
 int main(int argc, char const *argv[])
 {
-	//check_upgradeimg();
+    mySetTimeFrom4G();
+	//downloadFileFromTianyiYun("iotoos29000240327/ota/3/123/upgrade.img", "/tmp/upgrade.img");
+	//return 0;
+    char url[2048] = "127.0.0.1";
+    char domain[64] = {0};
+    char ip_addr[16] = {0};
+    int port = 80;
+    char file_name[256] = {0};
+ 
+    if (argc == 1)
+    {
+        OTA_INFO("Input a valid URL please\n");
+        //exit(0);
+    }
+    else{
+		if (strncasecmp((const char*)argv[1], "iotoos", 4) == 0)
+		{
+			downloadFileFromTianyiYun("iotoos29000240327/ota/1/upgrade.img", "/tmp/upgrade.img");
+		}else{
+			upgrade_from_card(argv[1]);
+			OTA_INFO("reboot -- p\n");
+			system("reboot -- p");
+			return 0;
+		}
+	}
+	
+	downloadFileFromTianyiYun("iotoos29000240327/ota/1/upgrade.img", "/tmp/upgrade.img");
+	//return 0;
+	struct statvfs buf2;
+    if (statvfs("/root", &buf2) == 0) {
+        unsigned long block_size = buf2.f_frsize; // 文件系统块大小
+        unsigned long available_blocks = buf2.f_bavail; // 可用块数量
+        unsigned long available_space = block_size * available_blocks;
+        
+        OTA_INFO("可用空间大小：%lu bytes\n", available_space);
+    } else {
+        OTA_INFO("无法获取/root分区信息\n");
+    }
+	struct sysinfo sys_info;
+
+    // 获取系统信息
+    if (sysinfo(&sys_info) != 0) {
+        perror("sysinfo");
+        return 1;
+    }
+
+    // 打印可用内存大小
+    OTA_INFO("Total RAM: %lu KB\n", sys_info.totalram / 1024);
+    OTA_INFO("Free RAM: %lu KB\n", sys_info.freeram / 1024);
+
+    // 打印可用Flash大小
+    OTA_INFO("Total Swap: %lu KB\n", sys_info.totalswap / 1024);
+    OTA_INFO("Free Swap: %lu KB\n", sys_info.freeswap / 1024);
+	
+
+
+// 主函数中的代码段
+long flash_space = getAvailableSpace();
+
+//if (flash_space < resp.content_length)
+{
+    OTA_ERR("Flash空间不足，无法完成升级flash_space %ldK\n",flash_space/1024);
+
+}
+    /*开新的线程下载文件*/
+    pthread_t parse_thread;
+    pthread_create(&parse_thread, NULL, parseAndextract, (void *) 0);
+    pthread_join(parse_thread, NULL);
+
+
+	OTA_INFO("reboot -- p\n");
+	system("reboot -- p");
+}
+
+int main2(int argc, char const *argv[])
+{
+	//downloadFileFromTianyiYun("iotoos29000240327/ota/1/upgrade.img", "/tmp/upgrade.img");
 	//return 0;
     char url[2048] = "127.0.0.1";
     char domain[64] = {0};
@@ -1068,8 +1606,7 @@ int main(int argc, char const *argv[])
     {
         OTA_INFO("Input a valid URL please\n");
         exit(0);
-    }
-    else{
+    }else{
 		if (strncasecmp((const char*)argv[1], "HTTP", 4) == 0)
 		{
 			strcpy(url, argv[1]);
@@ -1091,7 +1628,7 @@ int main(int argc, char const *argv[])
         OTA_INFO("can not get ip address\n");
         return 0;
     }
- 
+	
     puts("\n>>>>Detail<<<<");
     //设置http请求头信息
     char header[2048] = {0};
@@ -1178,9 +1715,9 @@ int main(int argc, char const *argv[])
         unsigned long available_blocks = buf2.f_bavail; // 可用块数量
         unsigned long available_space = block_size * available_blocks;
         
-        printf("可用空间大小：%lu bytes\n", available_space);
+        OTA_INFO("可用空间大小：%lu bytes\n", available_space);
     } else {
-        printf("无法获取/root分区信息\n");
+        OTA_INFO("无法获取/root分区信息\n");
     }
 	struct sysinfo sys_info;
 
@@ -1191,12 +1728,12 @@ int main(int argc, char const *argv[])
     }
 
     // 打印可用内存大小
-    printf("Total RAM: %lu KB\n", sys_info.totalram / 1024);
-    printf("Free RAM: %lu KB\n", sys_info.freeram / 1024);
+    OTA_INFO("Total RAM: %lu KB\n", sys_info.totalram / 1024);
+    OTA_INFO("Free RAM: %lu KB\n", sys_info.freeram / 1024);
 
     // 打印可用Flash大小
-    printf("Total Swap: %lu KB\n", sys_info.totalswap / 1024);
-    printf("Free Swap: %lu KB\n", sys_info.freeswap / 1024);
+    OTA_INFO("Total Swap: %lu KB\n", sys_info.totalswap / 1024);
+    OTA_INFO("Free Swap: %lu KB\n", sys_info.freeswap / 1024);
 	
 
 
